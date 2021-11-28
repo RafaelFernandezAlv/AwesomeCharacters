2674c41061db9fec2f6a8a10b176d7d2e758f707
Rafael Fernández <rafael.fernandez@alten.es>
2021-11-29 00:26:29 +0100

    Limit memory cache for Kingfisher

--- AwesomeCharacters/SceneDelegate.swift
+++ AwesomeCharacters/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -13,12 +14,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        configureKingfisher()
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
             window.rootViewController = UINavigationController(rootViewController: ListCharacterViewController())
             window.makeKeyAndVisible()
             self.window = window
         }
+    }
+    
+    func configureKingfisher() {
+        ImageCache.default.memoryStorage.config.totalCostLimit = 1024 * 1024 * 40 // 40 Mb max memory cache
     }
 }
 
